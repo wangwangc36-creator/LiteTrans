f82b308a33e656e9de9409726f5e00ed6088447d
wangwangc36-creator <wangwangc36@gmail.com>
2026-08-19 05:21:02 -0400

    Add runtime diagnostics protocol for alpha3

--- app/src/main/java/com/litetrans/module/util/Protocol.java
+++ app/src/main/java/com/litetrans/module/util/Protocol.java
@@ -9,12 +9,22 @@ public final class Protocol {
     public static final int MSG_WARMUP_RESULT = 4;
     public static final int MSG_CLEAR_CACHE = 5;
     public static final int MSG_CLEAR_CACHE_RESULT = 6;
+    public static final int MSG_STATS = 7;
+    public static final int MSG_STATS_RESULT = 8;
 
     public static final String KEY_TEXTS = "texts";
     public static final String KEY_TRANSLATIONS = "translations";
     public static final String KEY_OK = "ok";
     public static final String KEY_ERROR = "error";
+    public static final String KEY_HOST_PACKAGE = "host_package";
+    public static final String KEY_BATCH_COUNT = "batch_count";
+    public static final String KEY_TEXT_COUNT = "text_count";
+    public static final String KEY_CHANGED_COUNT = "changed_count";
+    public static final String KEY_LAST_HOST = "last_host";
+    public static final String KEY_LAST_SOURCE = "last_source";
+    public static final String KEY_LAST_TRANSLATION = "last_translation";
 
+    public static final String SELF_TEST_HOST = "__litetrans_selftest__";
     public static final String MODULE_PACKAGE = "com.litetrans.module";
     public static final String SERVICE_CLASS = "com.litetrans.module.service.TranslationService";
 }
