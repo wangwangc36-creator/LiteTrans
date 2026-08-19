d73356ea8c7133a2ee34360a6e124a544b4ac0ca
wangwangc36-creator <wangwangc36@gmail.com>
2026-08-19 05:58:34 -0400

    Alpha5: add Unhook type to Xposed API stub

--- xposed-stubs/src/main/java/de/robv/android/xposed/XC_MethodHook.java
+++ xposed-stubs/src/main/java/de/robv/android/xposed/XC_MethodHook.java
@@ -8,6 +8,10 @@ public abstract class XC_MethodHook {
     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {}
     protected void afterHookedMethod(MethodHookParam param) throws Throwable {}
 
+    public class Unhook {
+        public void unhook() {}
+    }
+
     public static class MethodHookParam {
         public Member method;
         public Object thisObject;
